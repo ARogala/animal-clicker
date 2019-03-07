653f4c68f315f836922d021331092017724e0553
ARogala <adrogala87@gmail.com>
2019-03-07 16:12:20 -0500

    change res.send to res.json on routes

--- controllers/animals_controller.js
+++ controllers/animals_controller.js
@@ -4,12 +4,15 @@ module.exports = {
 	getAnimal: function(req, res) {
 		const animalName = req.body.name;
 		Animal.findOne({ name: animalName }).then(animal => {
-			res.send(animal);
+			// res.send(JSON.stringify(animal));
+			res.json(animal);
 		});
 	},
 	getAllAnimals: function(req, res) {
 		Animal.find({}).then(animals => {
-			res.send(animals);
+			console.log(typeof animals);
+			// res.send(JSON.stringify(animals));
+			res.json(animals);
 		});
 	},
 	vote: function(req, res) {
@@ -17,7 +20,8 @@ module.exports = {
 		Animal.updateOne({ name: animalName }, { $inc: { clickCount: 1 } })
 			.then(() => Animal.findOne({ name: animalName }))
 			.then(animal => {
-				res.send(animal);
+				// res.send(JSON.stringify(animal));
+				res.json(animal);
 			});
 	}
 };
