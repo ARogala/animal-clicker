9cfab4e3c272b3cb6dde019d437adb42279a8c64
ARogala <adrogala87@gmail.com>
2019-03-09 15:19:38 -0500

    add getwinners endpoint to react proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,5 +3,6 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(proxy('/getanimal', { target: 'http://localhost:5000' }));
     app.use(proxy('/getallanimals', { target: 'http://localhost:5000' }));
+    app.use(proxy('/getwinners', { target: 'http://localhost:5000' }));
     app.use(proxy('/vote', { target: 'http://localhost:5000' }));
 };
