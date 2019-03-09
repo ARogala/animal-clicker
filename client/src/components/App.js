681b9ddc4b7f09c81dcb7808d83b759d82938492
ARogala <adrogala87@gmail.com>
2019-03-09 14:45:02 -0500

    add winners component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
 import AnimalVotes from './AnimalVotes';
+import Winners from './Winners';
 
 import logo from '../click.svg';
 
 class App extends Component {
-
     render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1>Animal Clicker</h1>
-                    <h2>Vote for your favorites!</h2>
-                </header>  
-                <AnimalVotes />
-            </div>
-        );
+        const dateNow = new Date();
+        const day = dateNow.getDay();
+        // const day = 0;
+        // console.log(day);
+        //display winners on Sunday
+        if (day === 0) {
+            return (
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <h1>Animal Clicker</h1>
+                        <h2>Vote for your favorites!</h2>
+                    </header>
+                    <Winners />
+                </div>
+            );
+        } else if (day !== 0) {
+            return (
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <h1>Animal Clicker</h1>
+                        <h2>Vote for your favorites!</h2>
+                    </header>
+                    <AnimalVotes />
+                </div>
+            );
+        }
     }
 }
 
---
+++ client/src/components/Winners.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Winners extends React.Component {
+	render() {
+		return(
+			<div>
+				<h1>This weeks winners are...</h1>
+			</div>
+		);
+	}
+}
+
+export default Winners;
