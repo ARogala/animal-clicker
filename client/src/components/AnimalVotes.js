3253f67ffb627b7cf767170addab374c6208a3ba
ARogala <adrogala87@gmail.com>
2019-03-09 16:19:45 -0500

    factor out getImageImportPaths from AnimalVotes.js

--- client/src/components/AnimalVotes.js
+++ client/src/components/AnimalVotes.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import toRegex from 'to-regex';
+
+import getImageImportPaths from '../importPaths';
 
 class AnimalVotes extends React.Component {
 	constructor(props) {
@@ -16,50 +17,13 @@ class AnimalVotes extends React.Component {
 			.then(
 				result => {
 					this.setState({ animals: result });
-					this.getImageImportPaths();
+					const imagePaths = getImageImportPaths(result);
+					this.setState({ imagePaths: imagePaths });
 				},
 				error => {
 					console.log(error);
 				}
 			);
-	}
-
-	getImageImportPaths() {
-		/*https://webpack.js.org/guides/dependency-management/
-        require.context() function takes 3 args
-        1. a directory to search
-        2. a flag whether subdirectories should be searched
-        3. a regular expression to match files against
-
-        A context module exports a (require) function that takes one argument: the request.
-        The exported function has 3 properties: resolve, keys, id
-            // console.log(typeof req);
-            // console.log(req);
-            // console.log(req.keys);
-            // //pass the key back into require to get the import
-            // console.log(req(req.keys()[0]));
-            // req.keys().forEach(function(key){
-            //     req(key);
-            //     console.log(req(key));
-            // });
-
-            once we have all import paths we can filter based
-            on a RegExp and build the DOM img elements
-        */
-		let animals = this.state.animals;
-		let req = require.context('../images', true, /.*\.jpg$/);
-		let filteredKeys = [];
-
-		for (let i = 0; i < animals.length; i++) {
-			const regex = toRegex(`./${animals[i].name}.jpg`);
-			const filteredKeysTemp = req.keys().filter(key => key.match(regex));
-			filteredKeys.push(filteredKeysTemp[0]);
-		}
-		//console.log(animals);
-		//console.log(filteredKeys);
-		const imagePaths = filteredKeys.map(key => req(key));
-		this.setState({ imagePaths: imagePaths });
-		//console.log(imagePaths);
 	}
 
 	vote(animalName) {
